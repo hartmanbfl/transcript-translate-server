a4caa403034e96c344500427e63939a669f13d0f
Ben Hartman <ben.hartman@unblu.com>
2024-02-21 19:30:02 +0100

    Missed one update in last commit

--- src/entity/DatabaseFile.entity.ts
+++ src/entity/DatabaseFile.entity.ts
@@ -1,9 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Tenant } from "./Tenant.entity.js";
 
-@Entity()
+@Entity({ name: "main_database_file"})
 export class DatabaseFile {
     @PrimaryGeneratedColumn("uuid")
     id: string;
+
+    @ManyToOne(() => Tenant)
+    @JoinColumn({ name: 'tenant_id' })
+    tenant: Tenant 
 
     @Column()
     filename: string;
