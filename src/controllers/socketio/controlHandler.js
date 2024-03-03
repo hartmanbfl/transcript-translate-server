726a5c0ed10cd803ca240987194a3c6836e84c5e
Ben Hartman <ben.hartman@unblu.com>
2024-03-03 11:52:49 +0100

    Merged in fix for transcript Undefined issue

--- src/controllers/socketio/controlHandler.js
+++ src/controllers/socketio/controlHandler.js
@@ -18,7 +18,7 @@ export const registerControlHandlers = (controlIo, clientIo, socket) => {
         const { serviceCode, transcript } = data;
 
         // Let all observers know that a new transcript is available
-        //        console.log(`Received a transcriptReady message`);
+        if (process.env.EXTRA_DEBUGGING) console.log(`Received a transcriptReady message: ${transcript}`);
         const transciptData = { serviceCode, transcript, serviceLanguageMap };
         transcriptAvailServiceSub.next(transciptData);
     });
